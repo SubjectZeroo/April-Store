23996d87bf50b68189f8e97f2bd10ba2870ea3df
Jafeht <jasordev@gmail.com>
2020-01-09 14:52:05 -0400

    actualizando el menu-item para trabajar con react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
-
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>Pagina de sombrero</h1>
+  </div>
+)
 function App() {
   return (
     <div>
-     <HomePage></HomePage>
+      <Switch>
+        <Route  exact path='/' component={ HomePage } />
+        <Route  path='/sombreros' component={ HatsPage } />
+      </Switch>
     </div>
   );
 }
