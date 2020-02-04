8268d212e64494dca19eb89e090db07af79bc08b
Jafeht <jasordev@gmail.com>
2020-02-03 20:50:28 -0400

    Actualizando la app con la pagina de productos, componente collection preview y componente collection item

--- src/App.js
+++ src/App.js
@@ -4,18 +4,14 @@ import './App.css';
 
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
 
-const HatsPage = () => (
-  <div>
-    <h1>Pagina de sombrero</h1>
-  </div>
-)
 function App() {
   return (
     <div>
       <Switch>
         <Route  exact path='/' component={ HomePage } />
-        <Route  path='/sombreros' component={ HatsPage } />
+        <Route  path='/shop' component={ ShopPage } />
       </Switch>
     </div>
   );
