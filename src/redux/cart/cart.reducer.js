1172f161a234aec44c50b084df4f8b2b90447ff7
Jafeht <jasordev@gmail.com>
2020-02-06 23:15:21 -0400

    agregando la funcionalidad addItem y removeItem para el componente checkout

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,6 +1,7 @@
 import CartActionTypes from './cart.types'
 
-import { addItemToCart } from './cart.utils'
+import { addItemToCart, removeItemFromCart} from './cart.utils'
+import CartItem from '../../components/cart-item/cart-item.component';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -20,6 +21,18 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
 
+      };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+        return {
+          ...state,
+          cartItems: state.cartItems.filter(
+            CartItem => CartItem.id != action.payload.id
+            )
+        };
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
       };
     default:
         return state
